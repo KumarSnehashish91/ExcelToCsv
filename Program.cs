5353b2646271e0349514afa82d7674003ce65638
KumarSnehashish91 <62428297+KumarSnehashish91@users.noreply.github.com>
2021-02-25 23:13:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string productsFilePath = "e:\\products.csv";
+            const string productsFilePath1 = "e:\\products2.csv";
+
+            DataTable dtProducts = ImportData.GetProducts();
+
+            //ExportData.ToCSV(dtProducts, productsFilePath);
+
+            ExportData.ToCSVWithFile(dtProducts, productsFilePath1);
+
+            //ExportData.GetProductList(dtProducts);
+
+            //ExportData.GetProductJson(dtProducts);
+        }
+    }
+}
